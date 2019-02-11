67e95227f01e62149c39e3986c2bf15c81f45f0e
wts6374 <wts6374@g.rit.edu>
2019-02-11 15:04:29 -0500

    Okay, now the level moves instead of the player, there is a low ceiling for gravity switching, a mock-up win/kill screen and a small thing for testing obstacles

---
+++ PlatformGrav/Assets/Scripts/Move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    // Use this for initialization
+    public bool stop;
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (stop == false)
+        {
+            transform.Translate(Vector3.left * 5 * Time.deltaTime);
+        }
+    }
+}
---
+++ PlatformGrav/Assets/Scripts/playerBox.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerBox : MonoBehaviour {
+
+    // Use this for initialization
+    public bool jump = false;
+   public bool stop = false;
+    public Camera cam;
+   public Rigidbody rb;
+  public int score;
+
+    public Text scoreText;
+    public Text winText;
+
+    public GameObject endScreen;
+
+    void Start () {
+        score = 0;
+
+        stop = false;
+        jump = false;
+
+        winText.text = "";
+
+        endScreen.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        scoreText.text = "Score: " + score.ToString();
+
+
+        if (stop == false)
+        {
+           
+            cam.transform.position = new Vector3(rb.transform.position.x + 1.0f, cam.transform.position.y, cam.transform.position.z);
+
+            if (Input.GetMouseButtonUp(0)&&jump==true)
+            {
+                rb.AddForce(new Vector3(0.0f, 3.0f, 0.0f) * 2.5f, ForceMode.Impulse);
+                jump = false;
+            }
+
+        }
+        else
+        {
+            cam.transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y, cam.transform.position.z);
+        }
+
+        endScreen.transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y,endScreen.transform.position.z);
+
+	}
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag=="Ground")
+        {
+            jump = true;
+        }
+
+        if (other.gameObject.tag == "End")
+        {
+            stop = true;
+            winText.text = "You Win!!!!";
+            Destroy(rb.gameObject);
+            endScreen.SetActive(true);
+        }
+
+        if (other.gameObject.tag == "Spike")
+        {
+            stop = true;
+            winText.color = Color.red;
+            winText.text = "You Lose";
+            Destroy(rb.gameObject);
+            endScreen.SetActive(true);
+        }
+
+        if (other.gameObject.tag == "Coin")
+        {
+            Destroy(other.gameObject);
+            score += 100;
+        }
+    }
+
+
+}
