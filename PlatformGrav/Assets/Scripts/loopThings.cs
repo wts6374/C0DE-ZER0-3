025cf091ba8da9265a6797ade1a463ab885790cd
wts6374 <wts6374@g.rit.edu>
2019-02-18 14:34:44 -0500

    Got coins going, just need obstacles

--- PlatformGrav/Assets/Scripts/loopThings.cs
+++ PlatformGrav/Assets/Scripts/loopThings.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class loopThings : MonoBehaviour {
 
+    public float fTimer = 0.2f;
+    public Transform coin;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,15 +14,28 @@ public class loopThings : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        //Will spawn in a new coin or obstacle constantly
+        fTimer -= Time.deltaTime;
+        if(fTimer<=0.0f)
+        {
+            fTimer = 0.2f;
+            Vector3 pos = new Vector3(500, Random.Range(-6, 11), -0.19f);
+            Instantiate(coin, pos, Quaternion.identity);
+        }
 	}
 
 
     private void OnCollisionEnter(Collision other)
     {
-        if ((other.gameObject.tag == "Ground" || other.gameObject.tag == "Coin" ))
+        if (other.gameObject.tag == "Ground")
         {
             other.transform.position = new Vector3(575, other.transform.position.y, other.transform.position.z);
+        }
+
+        if(other.gameObject.tag == "Coin")
+        {
+            Destroy(other.gameObject);
         }
 
     }
