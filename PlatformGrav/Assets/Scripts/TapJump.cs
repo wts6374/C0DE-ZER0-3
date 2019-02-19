09e6cc323a89fb384a69475881547bf21bc20740
ejp4604 <ejp4604@g.rit.edu>
2019-02-18 19:06:53 -0500

    Added image in canvas so that you can now click and tap to jump (click for testing on comp). Only works on clicking an area above the floor (for now)

--- PlatformGrav/Assets/Scripts/TapJump.cs
+++ PlatformGrav/Assets/Scripts/TapJump.cs
@@ -5,31 +5,24 @@ using UnityEngine;
 public class TapJump : MonoBehaviour {
  
     Vector3 impulseForce;
+    GameObject player;
 
 	// Use this for initialization
 	void Start () {
         // sets force for impulse
         impulseForce = new Vector3(0.0f, 3.0f, 0.0f) * 2.0f;
+        player = GameObject.Find("Player");
     }
 	
 	// Update is called once per frame
 	void Update () {
         TouchInput();
+        //ClickInput();
 	}
 
     void TouchInput()
     {
-        //if(Input.GetMouseButtonDown(0))
-        //{
-        //    // reverse gravity
-        //    Physics.gravity = -(Physics.gravity);
-        //    // zero out velocity
-        //    this.GetComponent<Rigidbody>().velocity = Vector3.zero;
-        //    // adds impulse force to increase speed of switching
-        //    this.GetComponent<Rigidbody>().AddForce(impulseForce, ForceMode.Impulse);
-        //    // reverse force for next gravity switch
-        //    impulseForce = -impulseForce;
-        //}
+
 
         // checks for touch input
         foreach (Touch touch in Input.touches)
@@ -45,13 +38,28 @@ public class TapJump : MonoBehaviour {
                     // reverse gravity
                     Physics.gravity = -(Physics.gravity);
                     // zero out velocity
-                    this.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                    player.GetComponent<Rigidbody>().velocity = Vector3.zero;
                     // adds impulse force to increase speed of switching
-                    this.GetComponent<Rigidbody>().AddForce(impulseForce, ForceMode.Impulse);
+                    player.GetComponent<Rigidbody>().AddForce(impulseForce, ForceMode.Impulse);
                     // reverse force for next gravity switch
                     impulseForce = -impulseForce;
                 }
             }
+        }
+    }
+
+    public void ClickInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // reverse gravity
+            Physics.gravity = -(Physics.gravity);
+            // zero out velocity
+            player.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            // adds impulse force to increase speed of switching
+            player.GetComponent<Rigidbody>().AddForce(impulseForce, ForceMode.Impulse);
+            // reverse force for next gravity switch
+            impulseForce = -impulseForce;
         }
     }
 }
