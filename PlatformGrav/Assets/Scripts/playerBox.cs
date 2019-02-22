43d63a53286b2a8e347ef2adeaf96fe82f371f5f
ejp4604 <ejp4604@g.rit.edu>
2019-02-21 21:53:35 -0500

    Added coin magnet

--- PlatformGrav/Assets/Scripts/playerBox.cs
+++ PlatformGrav/Assets/Scripts/playerBox.cs
@@ -3,33 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class playerBox : MonoBehaviour {
+public class playerBox : MonoBehaviour
+{
 
     // Use this for initialization
     public bool jump = false;
-   public bool stop = false;
+    public bool stop = false;
     public Camera cam;
-   public Rigidbody rb;
-  public float score;
-
+    public Rigidbody rb;
+    public float score;
+    public bool magnet = false;
     public Text scoreText;
     public Text winText;
-
+    public float timer = 0.0f;
     public GameObject endScreen;
 
-    void Start () {
+    void Start()
+    {
         score = 0;
         //Camera.main.transform.position.x = gameObject.transform.position.x + 2;
         stop = false;
         jump = false;
-
+        //magnet = true;
         winText.text = "";
 
         endScreen.SetActive(false);
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        RaycastHit hit;
+        if (magnet == true)
+        {
+            if (Physics.SphereCast(transform.position, 10, Vector3.right, out hit, 10))
+            {
+                if (hit.transform.name.Contains("Coin"))
+                {
+                    Destroy(hit.transform.gameObject);
+                    Debug.Log("coin");
+                }
+            }
+            timer += Time.deltaTime;
+            if (timer > 10.0f)
+            {
+                magnet = false;
+                timer = 0;
+            }
+        }
 
         //scoreText.text = "Score: " + score.ToString();
 
@@ -53,13 +77,13 @@ public class playerBox : MonoBehaviour {
 
         cam.transform.position = new Vector3(this.transform.position.x, cam.transform.position.y, cam.transform.position.z);//cam.transform.position.x, cam.transform.position.y, cam.transform.position.z);
 
-        endScreen.transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y,endScreen.transform.position.z);
+        endScreen.transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y, endScreen.transform.position.z);
 
-	}
+    }
 
     private void OnCollisionEnter(Collision other)
     {
-        if(other.gameObject.tag=="Ground")
+        if (other.gameObject.tag == "Ground")
         {
             jump = true;
         }
@@ -72,34 +96,31 @@ public class playerBox : MonoBehaviour {
             endScreen.SetActive(true);
         }
 
-        if (other.gameObject.tag == "Spike")
+        if (other.gameObject.name.Contains("Spike"))
         {
             //stop = true;
             //winText.color = Color.red;
             //winText.text = "You Lose";
             //Destroy(rb.gameObject);
             //endScreen.SetActive(true);
-            MySceneManager.Instance.coins = 0;
             MySceneManager.Instance.score = 0;
             MySceneManager.Instance.multiplier = 0;
-            Physics.gravity = new Vector3(0, -9.81f, 0);
+            MySceneManager.Instance.multiplier = 0;
             this.GetComponent<MainMenu>().Menuetwo();
         }
 
-     
-
         if (other.gameObject.tag == "Coin")
         {
-            
             //Destroy(other.gameObject);
 
             //plus 40 to score if you pick up a coin
             //score += 40;
         }
 
-        if (other.gameObject.tag == "SlowAbility")
+        if (other.gameObject.name == "mag")
         {
-           
+            Destroy(other.gameObject);
+            magnet = true;
         }
 
     }
