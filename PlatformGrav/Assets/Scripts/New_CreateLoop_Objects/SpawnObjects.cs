17854862c57bcf019576fcb94bf6adea36c0867b
ejp4604 <ejp4604@g.rit.edu>
2019-02-28 19:41:56 -0500

    Manual merge to ChangeOver branch

--- PlatformGrav/Assets/Scripts/New_CreateLoop_Objects/SpawnObjects.cs
+++ PlatformGrav/Assets/Scripts/New_CreateLoop_Objects/SpawnObjects.cs
@@ -10,48 +10,58 @@ public class SpawnObjects : MonoBehaviour {
     public GameObject slow;
     public GameObject mag;
 
-    List<GameObject> objects;
+    public List<GameObject> objects;
 
-    
-     
 
-	// Use this for initialization
-	void Start () {
+    float width;
+    float height;
+    float centerX;
+
+    // Use this for initialization
+    void Start () {
 
         Vector2 looping = GetComponent<RectTransform>().position;
         //gameObject.transform.position = new Vector2(looping.x, transform.position.y);
-        float width = 100f;
-        float height = 4f;
+        width = 100f;
+        height = 4f;
 
-        float centerX = GetComponent<RectTransform>().position.x;
+        centerX = GetComponent<RectTransform>().position.x;
         objects = new List<GameObject>();
-        for(int x = 0; x < 30; x++)
-        {
-            
 
-            int num = Random.Range(0, 5);
-            
-
-            if (num == 0)
-                Instantiate(spike, new Vector3(centerX + Random.Range(0, width), 0 + Random.Range(-height, height) , 0), Quaternion.identity);
-            else if (num == 1)
-                Instantiate(coin, new Vector3(centerX + Random.Range(0, width), 0 + Random.Range(-height, height), 0), Quaternion.identity);
-            else if (num == 2)
-                Instantiate(ground, new Vector3(centerX + Random.Range(0, width), 0 + Random.Range(-height, height), 0), Quaternion.identity);
-            else if (num == 3)
-                Instantiate(slow, new Vector3(centerX + Random.Range(0, width), 0 + Random.Range(-height, height), 0), Quaternion.identity);
-            else if (num == 4)
-                Instantiate(mag, new Vector3(centerX + Random.Range(0, width), 0 + Random.Range(-height, height), 0), Quaternion.identity);
-        }
+        for (int x = 0; x < 30; x++)
+            objects.Add(SpawningObjects());
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		for(int x = 0; x < 30; x++)
+        {
+            if (objects[x] == null)
+            {
+                Debug.Log("Missing: " + x);
+                objects[x] = SpawningObjects();
+            }
+        }
 	}
 
-    void SpawningObjects()
+    GameObject SpawningObjects()
     {
+        int num = Random.Range(0, 5);
 
+        GameObject temp = null;
+
+        if (num == 0)
+            temp = Instantiate(spike, new Vector3(centerX + Random.Range(0, width), 0 + Random.Range(-height, height), 0), Quaternion.identity);
+        else if (num == 1)
+            temp = Instantiate(coin, new Vector3(centerX + Random.Range(0, width), 0 + Random.Range(-height, height), 0), Quaternion.identity);
+        else if (num == 2)
+            temp = Instantiate(ground, new Vector3(centerX + Random.Range(0, width), 0 + Random.Range(-height, height), 0), Quaternion.identity);
+        else if (num == 3)
+            temp = Instantiate(slow, new Vector3(centerX + Random.Range(0, width), 0 + Random.Range(-height, height), 0), Quaternion.identity);
+        else if (num == 4)
+            temp = Instantiate(mag, new Vector3(centerX + Random.Range(0, width), 0 + Random.Range(-height, height), 0), Quaternion.identity);
+
+        return temp;
+        
     }
 }
